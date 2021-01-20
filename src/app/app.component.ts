504213ec95e67363611efd2d5556838494a9e134
Silas Santana <silas.santos@lojainterativa.com>
2021-01-20 10:46:23 -0300

    Tarefa 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+  serverName: string = '';
+  serverContent: string = ''
+  servers = []
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  addServer() {
+    this.servers.push({ type: 1, name: this.serverName, content: this.serverContent });
+    console.log(this.servers);
+  }
+
+  addBlueprint() {
+    this.servers.push({ type: 2, name: this.serverName, content: this.serverContent });
+  }
+
 }
